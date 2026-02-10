276a24dc3a136afb0a4922ed7e0e42664a88412b
Annyfee <2287551746@qq.com>
2026-02-10 17:55:47 +0800

    fix:修复mcp运行环境，确保mcp环境获取;session_id由RunnableConfig显式获取，不再由ai自己指定

--- tools/registry.py
+++ tools/registry.py
@@ -1,3 +1,6 @@
+import os
+
+from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from loguru import logger
@@ -9,23 +12,28 @@ global_rag_store = RAGStore()
 
 # [新增 3] 定义 RAG 检索工具 (给 Agent 查库用)
 @tool
-def search_knowledge_base(query: str):
+def search_knowledge_base(query: str,config:RunnableConfig): # 声明使用RunnableConfig来提取我们最初定义的thread_id
     """
     当系统提示'资料已存入知识库'时，或者需要回答基于事实的问题时，
     必须调用此工具从本地知识库(RAG)中检索。
     """
-    logger.info(f"📚 Agent 正在查询知识库: {query}")
-    return global_rag_store.query_formatted(query)
+    session_id = config.get("configurable",{}).get("thread_id","default_session")
+    logger.info(f"📚 Agent 正在查询知识库: {query} | Session_ID: {session_id}")
+    return global_rag_store.query_formatted(query,session_id)
 
 # 加载所有工具
 async def load_all_tools():
     """
     初始化并返回所有可用工具列表(MCP+RAG)
     """
+    # 动态地址获取 docker环境会自动注入MCP_HOST
+    mcp_host = os.getenv("MCP_HOST","127.0.0.1") #
+    mcp_url = f"http://{mcp_host}:8003/mcp"
+
     mcp_config = {
         "搜索服务": {
             "transport": "http",
-            "url": "http://127.0.0.1:8003/mcp"
+            "url": mcp_url,
         }
     }
     logger.info("🔌 正在连接 MCP 服务器...")
